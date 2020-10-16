5b428d8c9dbfc5c8b6ca48083ea4bb59ad73b1bd
bxine <sine.narisa@gmail.com>
2020-10-16 18:08:30 +0700

    format document and delete some comment

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import couchbase from 'couchbase';
+import express from "express";
+import couchbase from "couchbase";
 
 const cluster = new couchbase.Cluster("couchbase://127.0.0.1", {
   username: "Administrator",
@@ -11,48 +11,48 @@ const coll = bucket.defaultCollection();
 
 const app = express();
 
-var bodyParser = require('body-parser')
-app.use(bodyParser.json())
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 type todoInterface = {
-    "id": number,
-    "text": string,
-    "complete": boolean
-}
+  id: number;
+  text: string;
+  complete: boolean;
+};
 
 let number = 0;
 
-let todoRepositry: todoInterface[] = []
+let todoRepositry: todoInterface[] = [];
 
 // upsert data to db
 const upsertDocument = async (doc: todoInterface) => {
-    try {
-      const key = `${doc.id}`;
-      const result = await coll.upsert(key, doc);
-      //console.log('aaaa'+result);
-      return result
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    const key = `${doc.id}`;
+    const result = await coll.upsert(key, doc);
+    console.log('aaaa'+result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // get data from db
 const getData = async (key: String) => {
-    try {
-      const result = await coll.get(key);
-      console.log('aaaa'+result);
-      return result
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    const result = await coll.get(key);
+    console.log("aaaa" + result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // remove data from db
 const removeData = async (key: String) => {
   try {
     const result = await coll.remove(key);
-    //console.log('aaaa'+result)
-    return result
+    console.log('aaaa'+result)
+    return result;
   } catch (error) {
     console.error(error);
   }
@@ -61,14 +61,14 @@ const removeData = async (key: String) => {
 // update data
 const updateData = async (key: String, doc: todoInterface) => {
   try {
-    const text = `${doc.text}`
-    const complete = `${doc.complete}`
+    const text = `${doc.text}`;
+    const complete = `${doc.complete}`;
     const result = await coll.mutateIn(key, [
       couchbase.MutateInSpec.replace("text", text),
       couchbase.MutateInSpec.replace("complete", complete),
     ]);
-    console.log('bbbbb'+result);
-    return result
+    console.log("bbbbb" + result);
+    return result;
   } catch (error) {
     console.error(error);
   }
@@ -79,11 +79,11 @@ const getAllData = async () => {
   const query = ` SELECT * FROM todo `;
   try {
     let data = await cluster.query(query);
-    console.log(data)
+    console.log(data);
     let result = data.rows.map((row: object) => {
       return row;
     });
-    return result
+    return result;
   } catch (error) {
     console.error(error);
   }
@@ -96,77 +96,76 @@ const ftsMatchPhrase = async (phrase: any) => {
       "text",
       couchbase.SearchQuery.matchPhrase(phrase),
       { limit: 10 }
-    )
+    );
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
-app.get('/search', (req, res) => {
+app.get("/search", (req, res) => {
   (async () => {
     const query = req.query.q;
-    const todo = await ftsMatchPhrase(query)
-    if(!todo) return res.status(400).send({ msg: "mai mee ja" });
-    const result = todo.rows.map((e: any) => {return e})
-    //console.log(result);
+    const todo = await ftsMatchPhrase(query);
+    if (!todo) return res.status(400).send({ msg: "mai mee ja" });
+    const result = todo.rows.map((e: any) => {
+      return e;
+    });
     res.send(result);
-})()
+  })();
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   (async () => {
     const todo = await getAllData();
-    console.log(todo)
+    console.log(todo);
     res.send(todo);
-})()
+  })();
 });
 
-app.get('/:id', (req, res) => {
-    const id = req.params.id;
-    (async () => {
-        const todo = await getData(id);
-        console.log(todo)
-        if (!todo) return res.status(400).send({msg: 'error ja'});
-        res.send(todo.value);
-    })()
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  (async () => {
+    const todo = await getData(id);
+    if (!todo) return res.status(400).send({ msg: "error ja" });
+    res.send(todo.value);
+  })();
 });
 
-app.post('/', (req, res) => {
-    const {id, text, complete} = req.body
-    number += 1;
-    if(id || complete != undefined) return res.status(400).send({msg: 'error ja'});
-    const todoRepositry = {
-        "id": number,
-        "text": text,
-        "complete": false
-    };
-    upsertDocument(todoRepositry);
-    res.send(todoRepositry);
+app.post("/", (req, res) => {
+  const { id, text, complete } = req.body;
+  number += 1;
+  if (id || complete != undefined)
+    return res.status(400).send({ msg: "error ja" });
+  const todoRepositry = {
+    id: number,
+    text: text,
+    complete: false,
+  };
+  upsertDocument(todoRepositry);
+  res.send(todoRepositry);
 });
 
-app.delete('/:id', (req, res) => {
+app.delete("/:id", (req, res) => {
   const id = req.params.id;
   (async () => {
     const todo = await removeData(id);
-    console.log(todo)
-    if (!todo) return res.status(400).send({msg: 'error ja'});
-    res.send({msg: 'delete ja'});
-})()
+    if (!todo) return res.status(400).send({ msg: "error ja" });
+    res.send({ msg: "delete ja" });
+  })();
 });
 
-app.patch('/:id', (req, res) => {
-  const {id, text, complete, exist} = req.body;
+app.patch("/:id", (req, res) => {
+  const { id, text, complete, exist } = req.body;
   const n = req.params.id;
   (async () => {
     const todo = await getData(n);
-    if(!todo) return res.status(400).send({msg: 'error ja eiei'});
-    console.log('cccccccccccc'+todo)
-    todo.content.exist = exist || todo.exist
-    todo.content.text = text || todo.text
-    todo.content.complete = complete != undefined ? complete: todo.complete
+    if (!todo) return res.status(400).send({ msg: "error ja eiei" });
+    todo.content.exist = exist || todo.exist;
+    todo.content.text = text || todo.text;
+    todo.content.complete = complete != undefined ? complete : todo.complete;
     updateData(n, todo.value);
     res.send(todo.value);
-  })()
+  })();
 });
 
-app.listen(8000, () => console.log('Surver running'));
+app.listen(8000, () => console.log("Surver running"));

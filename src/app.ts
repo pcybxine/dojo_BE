c8a09a5ba7b4cb632d6043403ea57b8287c2b14d
bxine <sine.narisa@gmail.com>
2020-10-16 17:54:44 +0700

    Add get/remove/update/search to connect db

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,95 @@ let number = 0;
 
 let todoRepositry: todoInterface[] = []
 
+// upsert data to db
+const upsertDocument = async (doc: todoInterface) => {
+    try {
+      const key = `${doc.id}`;
+      const result = await coll.upsert(key, doc);
+      //console.log('aaaa'+result);
+      return result
+    } catch (error) {
+      console.error(error);
+    }
+};
+
+// get data from db
+const getData = async (key: String) => {
+    try {
+      const result = await coll.get(key);
+      console.log('aaaa'+result);
+      return result
+    } catch (error) {
+      console.error(error);
+    }
+};
+
+// remove data from db
+const removeData = async (key: String) => {
+  try {
+    const result = await coll.remove(key);
+    //console.log('aaaa'+result)
+    return result
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// update data
+const updateData = async (key: String, doc: todoInterface) => {
+  try {
+    const text = `${doc.text}`
+    const complete = `${doc.complete}`
+    const result = await coll.mutateIn(key, [
+      couchbase.MutateInSpec.replace("text", text),
+      couchbase.MutateInSpec.replace("complete", complete),
+    ]);
+    console.log('bbbbb'+result);
+    return result
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// get all data
+const getAllData = async () => {
+  const query = ` SELECT * FROM todo `;
+  try {
+    let data = await cluster.query(query);
+    console.log(data)
+    let result = data.rows.map((row: object) => {
+      return row;
+    });
+    return result
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// search from text
+const ftsMatchPhrase = async (phrase: any) => {
+  try {
+    return await cluster.searchQuery(
+      "text",
+      couchbase.SearchQuery.matchPhrase(phrase),
+      { limit: 10 }
+    )
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+app.get('/search', (req, res) => {
+  (async () => {
+    const query = req.query.q;
+    const todo = await ftsMatchPhrase(query)
+    if(!todo) return res.status(400).send({ msg: "mai mee ja" });
+    const result = todo.rows.map((e: any) => {return e})
+    //console.log(result);
+    res.send(result);
+})()
+});
+
 app.get('/', (req, res) => {
     res.send(todoRepositry);
 });
